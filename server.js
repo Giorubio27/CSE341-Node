d544b8e6790caaf37406ee8a520584c69c22fced
Giorubio27 <Giorubio27@gmail.com>
2026-05-13 08:56:07 -0600

    cont

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(process.env.port || 3000);
-console.log('Web server is running at port' + (process.env.port || 3000));  
+const port = 3000
+
+app.listen(process.env.port || port);
+console.log('Web server is running at port' + (process.env.port || port));  
 
